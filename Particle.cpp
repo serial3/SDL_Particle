e89a958f3f0163b34384e423d107a34cbe5776f1
serial3 <diogo_R_Leal@hotmail.com>
2023-01-03 02:41:18 +0000

    explosion centered

--- Particle.cpp
+++ Particle.cpp
@@ -1,16 +1,13 @@
 #include "Particle.h"
 #include <stdlib.h>
 #include "Screen.h"
+#include <math.h>
 
 namespace sdlparticle {
 	
-Particle::Particle(){
-	m_x = (2.0 * rand())/RAND_MAX - 1;
-	m_y = (2.0 * rand())/RAND_MAX - 1;
-
-	m_xspeed = 0.0001 * (((2.0 * rand())/RAND_MAX) -1);
-	m_yspeed = 0.0001 * (((2.0 * rand())/RAND_MAX) -1);
-
+Particle::Particle(): m_x(0), m_y(0) {
+	m_direction = (2 * M_PI * rand())/RAND_MAX;
+	m_speed = (0.001 * rand())/RAND_MAX;
 }
 
 Particle::~Particle(){
@@ -18,16 +15,12 @@ Particle::~Particle(){
 }
 
 void Particle::update() {
+	double xspeed = m_speed * cos(m_direction);
+	double yspeed = m_speed * sin(m_direction);
 
-	m_x += m_xspeed;
-	m_y += m_yspeed;
+	m_x = xspeed;
+	m_y = yspeed;
 
-	if(m_x < -1.0 || m_x >= 1.0) {
-		m_xspeed = -m_xspeed;
-	}
-	if(m_y < -1.0 || m_y >= 1.0) {
-		m_yspeed = -m_yspeed;
-	}
 }
 
 } // namespace sdlpractice
